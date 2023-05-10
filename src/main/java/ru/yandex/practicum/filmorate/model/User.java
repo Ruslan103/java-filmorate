4a86187fb6f49f7692c7afba89f4edb389f43155
Ruslan103 <117119513+Ruslan103@users.noreply.github.com>
2023-05-11 00:19:21 +0400

    первая итерация ТЗ 9 0:19

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import java.time.LocalDate;
+
 @Data
 public class User {
     private int id;
@@ -10,6 +11,7 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+
     public User(String email, String login, String name, LocalDate birthday) {
         this.email = email;
         this.login = login;
