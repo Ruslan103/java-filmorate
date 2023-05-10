ea5d9a0f72409ce4378e098ca89a9ae656ff9f78
Ruslan103 <117119513+Ruslan103@users.noreply.github.com>
2023-05-11 01:00:36 +0400

    первая итерация ТЗ 9 0:19

--- src/main/java/ru/yandex/practicum/filmorate/model/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FilmController.java
@@ -2,10 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,7 +15,7 @@ public class FilmController {
     HashMap<Integer, Film> films = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @PostMapping("/films/add")
+    @PostMapping("/films")
     public Film addFilm(@RequestBody Film film) {
         LocalDate releaseDate = LocalDate.of(1895, 12, 28);
         int maxDescription = 200;
@@ -45,7 +42,7 @@ public class FilmController {
         return film;
     }
 
-    @PostMapping("/films/update")
+    @PutMapping("/films")
     public Film updateFilm(@RequestBody Film film) {
         int id = film.getId();
         if (films.containsKey(id)) {
--- src/main/java/ru/yandex/practicum/filmorate/model/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/UserController.java
@@ -2,10 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,7 +15,7 @@ public class UserController {
     HashMap<Integer, User> users = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @PostMapping("/users/add")
+    @PostMapping("/users")
     public User addUser(@RequestBody User user) {
         if (user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
             logger.error("Неверный email: {}", user.getEmail());
@@ -43,7 +40,7 @@ public class UserController {
         return user;
     }
 
-    @PostMapping("/users/update")
+    @PutMapping("/users")
     public User updateUser(@RequestBody User user) {
         int id = user.getId();
         if (users.containsKey(id)) {
